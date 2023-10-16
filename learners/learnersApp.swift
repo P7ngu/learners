8ebfaf8700e138c0c82f3bd1f4efbe2525e1163c
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-10-16 10:12:02 +0200

    Initial Commit

---
+++ learners/learnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnersApp.swift
+//  learners
+//
+//  Created by Matteo Perotta on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct learnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
